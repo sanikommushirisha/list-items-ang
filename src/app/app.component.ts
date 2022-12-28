5e0c33688ce03f0727168cf6e79ca95a18b93d39
sanikommushirisha <sanikommushirisha@gmail.com>
2022-12-27 17:37:10 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,14 @@ import { AppModalContainer } from './containers/app-modal/app-modal.container';
   styleUrls: ['./app.component.sass'],
 })
 export class AppComponent implements OnInit {
-  selectedItemTypeId: string;
+  selectedItemTypeId: string; // Item type being displayed - 'animals' or 'fruits & veg'
   itemTypes: ItemTypes = [
+    // Used to display items in tab pane or burger menu based on screensize
     { id: 'animals', label: 'ANIMALS' },
     { id: 'fruitveg', label: 'FRUITS & VEGGIES' },
   ];
   listItems: ListItems = [];
-  selectedItem: ListItem | undefined;
+  selectedItem: ListItem | undefined; //Selected Item(on click) to be displayed on modal
 
   constructor(
     public dialog: Dialog,
@@ -37,16 +38,8 @@ export class AppComponent implements OnInit {
     this.fetchAndSetListItems(itemTypeId);
   };
 
-  showModal = (itemId: number) => {
-    this.selectedItem = this.listItems.find(item => item.id === itemId);
-    this.dialog.open(AppModalContainer, {
-      data: this.selectedItem,
-      autoFocus: false,
-      maxWidth: '70vw',
-    });
-  };
-
   fetchAndSetListItems = (itemTypeId: string) => {
+    // Fetch and set list items on logo click or tab change or burger menu item click(in small devices)
     return this.itemService.get(itemTypeId).subscribe({
       next: listItems => {
         this.listItems = listItems;
@@ -55,6 +48,15 @@ export class AppComponent implements OnInit {
         this.listItems = [];
         this.snackBar.open(error.message, 'Dismiss');
       },
+    });
+  };
+
+  showModal = (itemId: number) => {
+    this.selectedItem = this.listItems.find(item => item.id === itemId);
+    this.dialog.open(AppModalContainer, {
+      data: this.selectedItem,
+      autoFocus: false,
+      maxWidth: '70vw',
     });
   };
 }
